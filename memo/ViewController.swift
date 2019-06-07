d9384ccd64bd9d8ef2a7de37ba8e5b4aa641a787
miniring <dreamhnt89@gmail.com>
2019-06-07 16:21:02 +0900

    Initial Commit

---
+++ memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memo
+//
+//  Created by 이정민 on 07/06/2019.
+//  Copyright © 2019 이정민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
